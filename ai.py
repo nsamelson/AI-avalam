3f75d90cf353299e56259ca281d22ead9a2a031c
Nicolas <salocin.sam@gmail.com>
2020-04-02 12:06:18 +0200

    Add files via upload

--- ai.py
+++ ai.py
@@ -1,10 +1,11 @@
 import json
 import pprint
 import time
+from random import *
 
-
-
-
+row = 0
+col = 4
+count =0
 output_file = open('move.json').read()
 output_json = json.loads(output_file) 
 
@@ -13,16 +14,47 @@ print(pprint.pformat(output_json))
 
 """ ici on check si on peut ajouter ou pas une pile sur une autre """
 
-if 0< len(output_json["game"][0][3]) <5 \
-    and 0 < len(output_json["game"][0][4])  <= 5 - len(output_json["game"][0][3]):
-    
-    output_json["game"][0][4].extend(output_json["game"][0][3]) #on ajoute a la suite de la liste la pile qu'on bouge
-    output_json["game"][0][3].clear()    #on clear la liste de la pile qu'on a bougé
-    print(pprint.pformat(output_json))      #formater visuellement quand on print (pas faire dans le json)
-    
-else:
-    print("false")
 
+def gamerules():
+
+    for a in range(row-1,row+2) :
+        if a >= 0:    
+            for b in range(col-1,col+2):
+                if b>=0:
+                
+                    if 0< len(output_json["game"][row][col]) <5 \
+                        and 0 < len(output_json["game"][a][b])  <= 5 - len(output_json["game"][row][col]) :
+                        if a == row and b== col:
+                            print(a,b,"false")
+                        else:
+                            
+                            output_json["game"][a][b].extend(output_json["game"][row][col]) #on ajoute a la suite de la liste la pile qu'on bouge
+                            output_json["game"][row][col].clear()    #on clear la liste de la pile qu'on a bougé
+                            output_json["moves"].append({
+                                                            "move": {
+                                                                "from": [row, col],
+                                                                "to": [a, b]
+                                                            },
+                                                            "message": "I'm Smart"
+                                                        })
+                            break
+                    else:
+                        print("false")
+                            
+    print(pprint.pformat(output_json))
+
+# for c in range (0,3):
+
+#     row = randint(0,8)
+#     col = randint(0,8)
+#     gamerules()
+# for c in range(0,8):
+#     for d in range(0,8):
+#         row = c
+#         col = d
+#         gamerules()
+
+gamerules()
 with open("move.json","w") as outfile:
     json.dump(output_json,outfile)  #dump le json
 print(outfile)
